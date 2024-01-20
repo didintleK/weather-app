6451d0d660f8ee5d4d61efdb070dcfdc35d68f0c
didintleK <didintlekgatle004@gmail.com>
2024-01-20 04:02:01 +0200

    Update weather details

--- src/script.js
+++ src/script.js
@@ -2,9 +2,39 @@ function showWeather(response) {
   let tempElement = document.querySelector("#current-temp");
   let temp = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.city;
+  let details = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
+  details.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  cityElement.innerHTML = response.data.city;
   tempElement.innerHTML = Math.round(temp);
+  timeElement.innerHTML = showDate(date);
+
+  console.log(response.data);
+}
+function showDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function citySearch(city) {
