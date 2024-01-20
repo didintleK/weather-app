0b9f9fca47d2807f7cc4d4f2e57092b911cc84e0
didintleK <didintlekgatle004@gmail.com>
2024-01-20 03:06:24 +0200

    Intergrated API

--- src/script.js
+++ src/script.js
@@ -1,9 +1,27 @@
+function showWeather(response) {
+  let tempElement = document.querySelector("#current-temp");
+  let temp = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  tempElement.innerHTML = Math.round(temp);
+}
+
+function citySearch(city) {
+  let apiKey = "3516ae46fb6a9tf90fe7770oaa6c4a92";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiURL).then(showWeather);
+}
+
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#form-search");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  citySearch(searchInput.value);
 }
 
 let searchForm = document.querySelector("#city-form");
 searchForm.addEventListener("submit", search);
+
+citySearch("Pretoria");
